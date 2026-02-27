77a0fd1a1f01be849ce5daed60a07022e4715909
Neel Patel <your.email@example.com>
2026-02-27 19:23:44 +0530

    file1 Commit

---
+++ File1.java
@@ -0,0 +1,77 @@
+//Basic practice of java 
+
+import java.util.*;
+
+public class File1 {
+
+    // public void printNum(){
+
+    // Scanner sc = new Scanner(System.in);
+    // System.out.print("Entera num: ");
+    // int num = sc.nextInt();
+
+    // System.out.println("The num is: " + num);
+
+    // }
+
+    // public static void main(String[] args){
+
+    // File1 pr = new File1();
+    // pr.printNum();
+
+    // }
+
+    public static void main(String[] args) {
+        int mark = 9;
+
+        // if(mark >= 90)
+        // {
+        // System.out.println("AA");
+        // }
+        // else if(mark >= 70 && mark < 90){
+        // System.out.print("BB");
+        // }
+        // else if(mark >= 40 && mark < 70)
+        // {
+        // System.out.print("CC");
+        // }
+        // else{
+        // System.out.print("Fail");
+        // }
+
+        // switch(mark)
+        // {
+        // case 69: System.out.println("Time");
+        // break;
+        // default: System.out.println("null"); break;
+        // }
+
+        // for(int i = 0; i<=5; i++)
+        // {
+        // for(int j = 0; j<=5; j++)
+        // {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // int i = 0;
+        // while (i != 9) {
+
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // int arr[] = new int[4];
+
+        // for(int i =0; i<5; i++)
+        // {
+        //     arr[i] = i;
+        //     System.out.println(arr[i]);            
+        // }
+        
+
+
+    }
+
+}
