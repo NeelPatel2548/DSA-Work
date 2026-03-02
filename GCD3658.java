764967041bfea03ff02c169ccb40e1a18fbb4a03
Neel Patel <neelpatel000700@gmail.com>
2026-03-02 19:08:33 +0530

    3658

---
+++ GCD3658.java
@@ -0,0 +1,15 @@
+public class GCD3658 {
+    
+     public static void main(String[] args) {
+        
+        int n = 5, odd = 0, even = 0;
+
+        System.out.println();
+
+
+     }
+}
+
+
+// Sum of first n even numbers = n(n+1)
+// Sum of first n odd numbers = n²
