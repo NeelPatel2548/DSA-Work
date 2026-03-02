0dfdae64d9e4998abb6dd448bcb76399f7917654
Neel Patel <neelpatel000700@gmail.com>
2026-03-02 18:15:15 +0530

    GCD Done

--- GCD.java
+++ GCD.java
@@ -1,24 +1,20 @@
 public class GCD {
-    
+
     public int GCD(int n1, int n2) {
         int gcd = 0;
 
-        for(int i = 1; i<n2; i++)
-        {
-            if(n1 % i == 0 && n2 % i == 0){
+        for (int i = 1; i <= Math.min(n2, n1); i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
 
-                System.err.println(i);
+                gcd = i;
 
             }
         }
-
-
         return gcd;
     }
 
-    public static void main(String[] args)
-    {
-        int n1 = 6, n2 = 12;
+    public static void main(String[] args) {
+        int n1 = 12, n2 = 6;
 
         GCD gcd = new GCD();
         int ret = gcd.GCD(n1, n2);
