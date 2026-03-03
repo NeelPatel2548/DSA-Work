0b903ec84a3c18d0e3a22f9f3a8c7e5401e467c7
Neel Patel <neelpatel000700@gmail.com>
2026-03-03 18:59:16 +0530

    PrintDivisior

---
+++ Printdivisior.java
@@ -0,0 +1,17 @@
+class Printdivisior{
+
+    public static void main(String[] args)
+    {
+        int n = 12;
+
+        for(int i = 1; i<=n; i++)
+        {
+            if(n % i == 0)
+            {
+                System.out.print(i + "  ");
+            }
+        }
+    }
+
+
+}
